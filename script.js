a408d2a13557aa852fb52f56d2f033c1d98e7c19
Andrade1221 <111130558+Andrade1221@users.noreply.github.com>
2023-10-04 11:15:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 function goToHome() {
   window.location.href = 'index.html';
 }
+
+function onYouTubeIframeAPIReady() {
+  var player;
+  player = new YT.Player('youtube-frame', {
+    videoId: 'GTiBuVb2nZ0',  // Substitua pelo ID real do vídeo
+    playerVars: {
+      'autoplay': 1,            // Inicia a reprodução automaticamente
+      'controls': 1,            // Exibe os controles do player
+      'showinfo': 0,            // Oculta informações do vídeo
+      'rel': 0,                 // Oculta vídeos relacionados
+    },
+    events: {
+      'onReady': onPlayerReady,
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  // Você pode executar ações quando o player estiver pronto, se necessário
+}
